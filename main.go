b05f24f0836397243d7183ccb0448d24650f28f4
= <=>
2025-03-26 17:11:32 +0200

    DRAFT

--- main.go
+++ main.go
@@ -13,9 +13,35 @@ package main
 */
 import "C"
 import (
+	"github.com/vmihailenco/msgpack/v5"
 	"fmt"
 	"unsafe"
 )
+
+// Q аналог Ruby `q`
+func Create(hash map[string]interface{}, asdf, qwerty *int) string {
+	if asdf == nil {
+		defaultVal := -1
+		asdf = &defaultVal
+	}
+	if qwerty == nil {
+		defaultVal := -1
+		qwerty = &defaultVal
+	}
+
+	// Серіалізація в MessagePack
+	data, err := msgpack.Marshal(hash)
+	if err != nil {
+		return ""
+	}
+
+	// Викликаємо C-функцію
+	ptr := C.__create((*C.uchar)(unsafe.Pointer(&data[0])), C.size_t(len(data)), (*C.int)(unsafe.Pointer(asdf)), (*C.int)(unsafe.Pointer(qwerty)))
+
+	// Конвертуємо C-строку в Go-строку
+	defer C.free(unsafe.Pointer(ptr))
+	return C.GoString(ptr)
+}
 
 func main() {
 	str := "fsdfsfsdfsdfsdfsdf"
@@ -26,6 +52,19 @@ func main() {
 
 	// Виклик функції з Rust
 	C.encrypted_key(cStr)
+
+	// Створюємо хеш (map)
+	data := map[string]interface{}{
+		"key1": "value1",
+		"key2": 42,
+		"key3": true,
+	}
+
+	// Викликаємо функцію Q
+	result := Create(data, nil, nil)
+
+	// Виводимо результат
+	fmt.Println("Result from Q:", result)
 
 	fmt.Println("Called Rust function yo successfully!")
 }
--- store_jt.h
+++ store_jt.h
@@ -14,6 +14,14 @@ extern "C" {
 // Оголошення функції з Rust
 void encrypted_key(const char *key);
 
+const char* __create(const uint8_t* data, size_t len, const int* ttl, const int* silence_read);
+
+const char* __read(const char* token);
+
+const int* __update(const char* token, const uint8_t* data, size_t len, const int* ttl, const int* silence_read);
+
+const int* __delete(const char* token);
+
 #ifdef __cplusplus
 }
 #endif
