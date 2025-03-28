1dd43c5e318838c0165f63724f5e514a7628a914
= <=>
2025-03-28 19:23:27 +0200

    added crud_jt.go wuth funcs create, read

---
+++ crud_jt.go
@@ -0,0 +1,78 @@
+package main
+
+/*
+#cgo linux,amd64 LDFLAGS: -L./native/linux/x86_64 -l store_jt -Wl,-rpath,.
+#cgo linux,arm64 LDFLAGS: -L./native/linux/arm64 -l store_jt -Wl,-rpath,.
+
+#cgo darwin,amd64 LDFLAGS: -L ./native/macos/x86_64 -l store_jt -Wl,-rpath,.
+#cgo darwin,arm64 LDFLAGS: -L./native/macos/arm64 -l store_jt -Wl,-rpath,.
+
+#cgo windows,amd64 LDFLAGS: -L./native/windows/x86_64 -l store_jt -Wl,-rpath,.
+#cgo windows,arm64 LDFLAGS: -L./native/windows/arm64 -l store_jt -Wl,-rpath,.
+#include "store_jt.h"
+*/
+import "C"
+import (
+	"github.com/vmihailenco/msgpack/v5"
+	"unsafe"
+  "encoding/json"
+  // "fmt"
+)
+
+// Q аналог Ruby `q`
+func Create(hash *map[string]interface{}, asdf, qwerty *int) string {
+	// var asdfVal, qwertyVal int
+
+  ttl_for_call := C.int64_t(-1)
+  silence_read_for_call := C.int32_t(-1)
+
+	// Якщо asdf == nil, створюємо змінну та передаємо її адресу
+  if asdf != nil {
+    asdf64 := int64(*asdf)
+
+		ttl_for_call = C.int64_t(asdf64)
+	}
+	if qwerty != nil {
+    qwerty32 := int32(*qwerty)
+
+		silence_read_for_call = C.int32_t(qwerty32)
+	}
+  //
+	// Сериализуем в MessagePack
+	data, err := msgpack.Marshal(*hash)
+	if err != nil {
+		return ""
+	}
+
+	// Викликаємо C-функцію
+	ptr := C.__create(
+		(*C.uchar)(unsafe.Pointer(&data[0])), C.size_t(len(data)),
+		ttl_for_call, silence_read_for_call,
+	)
+
+	// Конвертуємо C-строку в Go-строку
+	defer C.free(unsafe.Pointer(ptr))
+	return C.GoString(ptr)
+}
+
+// W аналог Ruby `w`
+func Read(value string) (map[string]interface{}, error) {
+	cValue := C.CString(value)
+	defer C.free(unsafe.Pointer(cValue))
+
+	ptr := C.__read(cValue)
+	if ptr == nil {
+		return nil, nil
+	}
+
+  // // Виводимо результат перед конвертацією
+	// rawStr := C.GoString(ptr)
+	// fmt.Println("Raw C Output:", rawStr) // Вивід на екран
+
+	defer C.free(unsafe.Pointer(ptr))
+
+	// Декодируем JSON
+	var result map[string]interface{}
+	err := json.Unmarshal([]byte(C.GoString(ptr)), &result)
+	return result, err
+}
--- main.go
+++ main.go
@@ -1,58 +1,11 @@
 package main
 
-/*
-#cgo linux,amd64 LDFLAGS: -L./native/linux/x86_64 -l store_jt -Wl,-rpath,.
-#cgo linux,arm64 LDFLAGS: -L./native/linux/arm64 -l store_jt -Wl,-rpath,.
-
-#cgo darwin,amd64 LDFLAGS: -L ./native/macos/x86_64 -l store_jt -Wl,-rpath,.
-#cgo darwin,arm64 LDFLAGS: -L./native/macos/arm64 -l store_jt -Wl,-rpath,.
-
-#cgo windows,amd64 LDFLAGS: -L./native/windows/x86_64 -l store_jt -Wl,-rpath,.
-#cgo windows,arm64 LDFLAGS: -L./native/windows/arm64 -l store_jt -Wl,-rpath,.
-#include "store_jt.h"
-*/
-import "C"
 import (
-	"github.com/vmihailenco/msgpack/v5"
+	"C"
 	"fmt"
-	"unsafe"
 )
 
-// Q аналог Ruby `q`
-func Create(hash map[string]interface{}, asdf, qwerty *int) string {
-	if asdf == nil {
-		defaultVal := -1
-		asdf = &defaultVal
-	}
-	if qwerty == nil {
-		defaultVal := -1
-		qwerty = &defaultVal
-	}
-
-	// Серіалізація в MessagePack
-	data, err := msgpack.Marshal(hash)
-	if err != nil {
-		return ""
-	}
-
-	// Викликаємо C-функцію
-	ptr := C.__create((*C.uchar)(unsafe.Pointer(&data[0])), C.size_t(len(data)), (*C.int)(unsafe.Pointer(asdf)), (*C.int)(unsafe.Pointer(qwerty)))
-
-	// Конвертуємо C-строку в Go-строку
-	defer C.free(unsafe.Pointer(ptr))
-	return C.GoString(ptr)
-}
-
 func main() {
-	str := "fsdfsfsdfsdfsdfsdf"
-
-	// Конвертація Go-рядка в C-string
-	cStr := C.CString(str)
-	defer C.free(unsafe.Pointer(cStr)) // Звільнення пам'яті після виклику
-
-	// Виклик функції з Rust
-	C.encrypted_key(cStr)
-
 	// Створюємо хеш (map)
 	data := map[string]interface{}{
 		"key1": "value1",
@@ -61,10 +14,13 @@ func main() {
 	}
 
 	// Викликаємо функцію Q
-	result := Create(data, nil, nil)
+	ttl := 100
+	silence_read := 17
+	result := Create(&data, &ttl, &silence_read)
 
 	// Виводимо результат
-	fmt.Println("Result from Q:", result)
+	fmt.Println("Result from Create:", result)
+	fmt.Println(Read(result))
 
 	fmt.Println("Called Rust function yo successfully!")
 }
